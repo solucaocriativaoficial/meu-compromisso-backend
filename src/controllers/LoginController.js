2b35952a290617428eef01fd9a45c655725fd84b
brunolgb <brunodscolado@gmail.com>
2020-03-02 16:17:34 -0400

    separando o CPF da SENHA, login em 2 passos

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -2,12 +2,12 @@ const connection_pg = require('../config/connection_pg');
 const error_handling = require('../utils/ErrorHandling');
 
 module.exports = {
-    validation(req, res){
-        const {cpf, password_access} = req.body;
+    validation_cpf(req, res){
+        const { cpf } = req.body;
 
         const comand = {
-            text: `SELECT * FROM Login WHERE cpf=$1 AND password_access=$2`,
-            values: [cpf, password_access]
+            text: `SELECT member FROM Login WHERE cpf=$1`,
+            values: [cpf]
         }
 
         connection_pg
@@ -18,7 +18,32 @@ module.exports = {
                 res.json(rows)
             }
             else{
-                res.json({ message: 'CPF ou Senha inválidos!', status: "ok" })
+                res.json({ message: 'CPF não cadastrado!', status: "ok" })
+            }
+        })
+        .catch(error => {
+            const status = "erro";
+            res.json({ message: "Erro na consulta!", status: status, complete_erro: error})
+            error_handling.getError(error);
+        })
+    },
+    validation_password(req, res){
+        const { member, password_access } = req.body;
+
+        const comand = {
+            text: `SELECT member, cpf, privileges_m FROM Login WHERE member=$1 AND password_access=$2`,
+            values: [member, password_access]
+        }
+
+        connection_pg
+        .query(comand)
+        .then(content => {
+            const { rowCount, rows } = content;
+            if(rowCount){
+                res.json(rows)
+            }
+            else{
+                res.json({ message: 'Senha inválida!', status: "ok" })
             }
         })
         .catch(error => {
