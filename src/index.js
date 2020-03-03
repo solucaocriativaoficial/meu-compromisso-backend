d3209bf9824fe6ea33a175cb23ebf7606296545b
brunolgb <brunodscolado@gmail.com>
2020-03-03 17:57:31 -0400

    usando o cors no index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(Routes);
