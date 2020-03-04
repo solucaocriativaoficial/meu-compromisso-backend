c28c25d4bf21f8a1ec3201a0271f1dde92767568
brunolgb <brunodscolado@gmail.com>
2020-03-03 22:43:50 -0400

    Mudando variaveis de ambiente

--- src/config/connection_pg.js
+++ src/config/connection_pg.js
@@ -1,11 +1,11 @@
 const { Pool, Client } = require('pg');
 
 const pool = new Pool({
-    user: process.env.USERNAME,
-    host: process.env.HOST,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
     database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 module.exports = pool;
--- src/config/connection_sequelize.js
+++ src/config/connection_sequelize.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
-    host: process.env.HOST,
-    dialect: process.env.DIALECT,
-    port: process.env.PORT
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
 })
 
 module.exports = sequelize;
