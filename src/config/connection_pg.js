bc7143cbdcadc3eb71d33239bdff223ec31e213a
brunolgb <brunodscolado@gmail.com>
2020-03-03 22:26:20 -0400

    Variaveis ambiente

--- src/config/connection_pg.js
+++ src/config/connection_pg.js
@@ -1,12 +1,11 @@
 const { Pool, Client } = require('pg');
 
-require('dotenv').config();
-
 const pool = new Pool({
     user: process.env.USERNAME,
     host: process.env.HOST,
     database: process.env.DATABASE,
-    password: process.env.PASSWORD
+    password: process.env.PASSWORD,
+    port: process.env.PORT
 });
 
 module.exports = pool;
--- src/config/connection_sequelize.js
+++ src/config/connection_sequelize.js
@@ -1,10 +1,9 @@
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
-
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
     host: process.env.HOST,
     dialect: process.env.DIALECT,
+    port: process.env.PORT
 })
 
 module.exports = sequelize;
