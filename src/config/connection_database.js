b3ebfef4ed99caf6ed0bb0dc278c48ec2f0e8040
brunolgb <brunodscolado@gmail.com>
2020-04-09 09:57:53 -0400

    tratando o erro de conexao

--- src/config/connection_database.js
+++ src/config/connection_database.js
@@ -1,14 +1,14 @@
 const Mongoose = require('mongoose')
 const ErroHandling = require('../utils/ErrorHandling')
 
-try {
-    Mongoose.connect(process.env.DATABASE_URL_MONGO,{
+Mongoose.connect(process.env.DATABASE_URL_MONGO,{
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
     })
-} catch (error) {
-    ErroHandling(error)
-}
+    .catch(error => {
+    console.log("Erro ao tentar se conectar ao banco de dados!", error)
+    //ErroHandling(error)
+})
 
 Mongoose.set('useCreateIndex', true);
 
